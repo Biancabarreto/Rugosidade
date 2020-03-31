42ed9cc995d8825d5ac090a340e237a57115f501
Biancabarreto <42420584+Biancabarreto@users.noreply.github.com>
2020-03-31 18:40:04 -0300

    eliminando fator

--- mcode_rugosidade_cilindro/mfiles/plot_teste_multiple.m
+++ mcode_rugosidade_cilindro/mfiles/plot_teste_multiple.m
@@ -1,10 +1,6 @@
-function plot_teste_multiple(DATA,OUTPUTDIR,FATOR)
+function plot_teste_multiple(DATA,OUTPUTDIR)
     graphics_toolkit gnuplot
 
-
-    FATORX=FATOR(1);
-    FATORY=FATOR(2);
-    FATORZ=FATOR(3);
 
     LL=64;
     xi =linspace(min(DATA(:,1)),max(DATA(:,1)),LL);
@@ -13,23 +9,23 @@ function plot_teste_multiple(DATA,OUTPUTDIR,FATOR)
     zzi = griddata(DATA(:,1), DATA(:,2), DATA(:,3), xxi, yyi);
 
     hfig1=figure(1);
-    %mesh(xxi,yyi,zzi*FATORZ)
-    surf(xxi*FATORX,yyi*FATORY,zzi*FATORZ)
+    %mesh(xxi,yyi,zzi)
+    surf(xxi,yyi,zzi)
     colormap(jet)
     print(gcf,fullfile(OUTPUTDIR,['img_all_mesh.png']),'-dpng');
     hgsave (hfig1,fullfile(OUTPUTDIR,['img_all_mesh.txt']))
 
 
     hfig2=figure(2);
-    scatter3(DATA(:,1)*FATORX, DATA(:,2)*FATORY, DATA(:,3)*FATORZ)
+    scatter3(DATA(:,1), DATA(:,2), DATA(:,3))
     print(gcf,fullfile(OUTPUTDIR,['img_all_scatter3.png']),'-dpng');
     hgsave (hfig2,fullfile(OUTPUTDIR,['img_all_scatter3.txt']))
 
     [MINIMO IDMIN]=min(DATA(:,3));
     [MAXIMO IDMAX]=max(DATA(:,3));
 
-    BLOCKDAT=[DATA(IDMIN,1)*FATORX, DATA(IDMIN,2)*FATORY, DATA(IDMIN,3)*FATORZ; ...
-              DATA(IDMAX,1)*FATORX, DATA(IDMAX,2)*FATORY, DATA(IDMAX,3)*FATORZ];
+    BLOCKDAT=[DATA(IDMIN,1), DATA(IDMIN,2), DATA(IDMIN,3); ...
+              DATA(IDMAX,1), DATA(IDMAX,2), DATA(IDMAX,3)];
 
     save('-ascii',fullfile(OUTPUTDIR,['min_max.txt']),'BLOCKDAT');
 endfunction
